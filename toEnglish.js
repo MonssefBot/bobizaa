b47eddd0d006806fe6501285e425554bb17c7b08
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-03-10 14:20:19 +0000

    Update toEnglish.js

--- toEnglish.js
+++ toEnglish.js
@@ -8,29 +8,28 @@ let handler = async (m, {
     text,
     command
 }) => {
-            if (!text) return m.reply("يستعمل هذا الأمر لترجمة النصوص الى اللغة الإنجليزية . مثلا\n*.trans إستعملت واتساب لترجمة النصوص*")
-            try {
-                let item = await Translate(text)
-                let cap = `🔍 *[ T R A N S L A T E ]*
+    if (!text) return m.reply("يستعمل هذا الأمر لترجمة النصوص الى اللغة الإنجليزية . مثلا\n*.trans إستعملت واتساب لترجمة النصوص*")
+    try {
+        let item = await Translate(text)
+        let cap = `🔍 *[ T R A N S L A T E ]*
 
 ✏️ *:* ${text}
 📌 *:* ${item}
 `
-                await conn.sendFile(m.chat, "https://telegra.ph/file/e9884546aaad4c9f47125.jpg", "", cap, m)
-                
-            } catch (e) {
-                await m.reply('error')
-            }
+        await conn.sendFile(m.chat, "https://telegra.ph/file/e9884546aaad4c9f47125.jpg", "", cap, m)
+    } catch (e) {
+        console.error(e)
+        await m.reply('حدث خطأ أثناء الترجمة')
+    }
 }
 handler.help = ["to-en"]
-handler.tags = ["to-en"]
-handler.command = /^(to-en|en)$/i
+handler.tags = ["search"]
+handler.command = /^(to-en)$/i
 export default handler
 
-function Translate(text, lang = "en") {
-  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${encodeURI(text)}`;
-  fetch(url).then((response) => response.json())
-  .then((json) => {
+async function Translate(text, lang = "en") {
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${lang}&dt=t&q=${encodeURI(text)}`;
+    let response = await fetch(url);
+    let json = await response.json();
     return json[0][0][0];
-  })
-      }
+            }
