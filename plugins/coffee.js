1e9b6ff8867e506593d4bba40fec9ee7402e8333
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-03-10 11:19:43 +0000

    Update coffee.js

--- plugins/coffee.js
+++ plugins/coffee.js
@@ -1,3 +1,35 @@
+import { Translate } from '@google-cloud/translate';
+import fs from 'fs';
+
+// يرجى تعيين متغيرات البيئة GOOGLE_APPLICATION_CREDENTIALS وGOOGLE_PROJECT_ID
+// إلى معرفاتك الخاصة بمشروع Google Cloud ومفتاح واجهة برمجة التطبيقات
+
+const translate = new Translate();
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'يُرجى إرسال نص للترجمة';
+
+  // الحصول على أول حرفين من الرسالة
+  const targetLang = text.substring(0, 2);
+  const textToTranslate = text.substring(2);
+
+  await conn.reply(m.chat, global.wait, m);
+
+  // ترجمة النص إلى اللغة المحددة
+  const [translation] = await translate.translate(textToTranslate, targetLang);
+  const translatedText = translation;
+
+  conn.reply(m.chat, translatedText, m);
+};
+
+handler.help = ['coffee'];
+handler.tags = ['tools'];
+handler.command = ['ترجم'];
+
+export default handler;
+
+
+/*
 let handler = async (m, {
     conn,
     isOwner,
@@ -16,4 +48,4 @@ let handler = async (m, {
 handler.help = ["coffee"]
 handler.tags = ["tools"]
 handler.command = /^(coffee)$/i
-export default handler
+export default handler*/
