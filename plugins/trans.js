2174396d02121f3ad258fde0cb60836ec3b7eebd
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-03-10 12:09:45 +0000

    Update and rename coffee.js to trans.js

--- plugins/trans.js
+++ plugins/trans.js
@@ -1,28 +1,28 @@
 import { Translate } from '@google-cloud/translate';
-import fs from 'fs';
 
 const translate = new Translate();
 let handler = async (m, { conn, text }) => {
   if (!text) throw "يُرجى إرسال نص للترجمة";
 
   try{
-  const targetLang = text.substring(0, 2);
-  const textToTranslate = text.substring(2);
-  //await conn.reply(m.chat, global.wait, m);
-  const [translation] = await translate.translate(textToTranslate, targetLang);
-  const translatedText = translation;
+    await m.reply(wait)
+    const targetLang = text.substring(0, 2);
+    const textToTranslate = text.substring(2);
+    //await conn.reply(m.chat, global.wait, m);
+    const [translation] = await translate.translate(textToTranslate, targetLang);
+    const translatedText = translation;
 
-  await conn.reply(m.chat, translatedText, m);
+    await conn.reply(m.chat, translatedText, m);
   }catch(e){
     throw eror
   }
 }
 
-handler.help = ["trans"];
-handler.tags = ["tools"];
+handler.help = ["trans"]
+handler.tags = ["tools"]
 handler.command = /^(trans)$/i
 
-export default handler;
+export default handler
 
 
 /*
