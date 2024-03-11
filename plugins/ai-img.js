cf95d57f7c3c5ca20cec43bc42a2ca48a8ff9a0a
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-03-11 09:18:31 +0000

    Update ai-img.js

--- plugins/ai-img.js
+++ plugins/ai-img.js
@@ -1,8 +1,8 @@
-import fetch from 'node-fetch';
-import OpenAI from 'openai';
+/*import fetch from 'node-fetch';
+import OpenAI from 'openai';*/
 
-/*const fetch = require('node-fetch');
-const OpenAI = require('openai');*/
+const fetch = require('node-fetch');
+const OpenAI = require('openai');
 
 let handler = async (m, {
     conn,
@@ -11,12 +11,10 @@ let handler = async (m, {
     text,
     command
 }) => {
-    if (!text) return m.reply("يستعمل هذا الأمر لتوليد صور HD بواسطة الذكاء الإصطناعي مثلا . مثلا\n*.img إستعملت واتساب لترجمة النصوص*");
+    if (!text) return m.reply("يستعمل هذا الأمر لتوليد صور HD بواسطة الذكاء الإصطناعي . مثلا\n*.img A photograph of a white Siamese cat*");
     await m.reply(wait)
     try {
         const openai = new OpenAI();
-
-
         const imgs = await openai.createImage({
             model: "dall-e-3",
             prompt: text,
@@ -25,11 +23,9 @@ let handler = async (m, {
         });
         const imageUrl = imgs.data.data[0].url;
 
-        const t = JSON.stringify(imgs);
-
-        await m.reply(t);
+        //const t = JSON.stringify(imgs);
+        await m.reply(imageUrl);
     } catch (e) {
-        
         await m.reply('*حدث خطأ أثناء إنشاء الصور*❎');
     }
 }
@@ -38,35 +34,28 @@ handler.tags = ["ai"]
 handler.command = /^(img|img-ai)$/i
 export default handler
 
-
 async function Translate(text) {
     const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURI(text)}`;
     let response = await fetch(url);
     let json = await response.json();
     return json[0][0][0];
 }
-    //const openaiUrl = 'https://api.openai.com/v1/images/generations';
-    //const openaiKey = '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';
-
-
-
-
-
-
-    
-    /*const methods = {
+async function Send(text){
+    const openaiUrl = 'https://api.openai.com/v1/images/generations';
+    const openaiKey = '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';
+    const methods = {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${openaiKey}`
         },
         body: JSON.stringify({
-          "prompt": textT,
+          "prompt": text,
           "n": 1,
           "size": "1024x1024"
         })
       };
-
     const res = await fetch(openaiUrl, methods);
     const data = await res.json();
-    return data.data.data[0].url;*/
+    return data.data.data[0].url;
+}
