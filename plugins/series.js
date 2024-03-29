73e905ef70aeffe1336ee65506693b7283932205
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-03-29 13:28:47 +0000

    Update series.js

--- plugins/series.js
+++ plugins/series.js
@@ -12,7 +12,7 @@ let handler = async (m, {
             await m.reply(wait)
             try {
                 let item = await Wiki(text)
-                let cap = item[0];
+                let cap = item;
                 await conn.sendFile(m.chat, "https://telegra.ph/file/6ebc06f2b66e93e18155f.jpg", "", cap, m)
                 
             } catch (e) {
@@ -28,12 +28,28 @@ async function Wiki(query) {
   const res = await fetch(`https://mycima.wecima.show/search/${query}/list/`);
   const html = await res.text();
   const $ = cheerio.load(html);
-  let linksArray = ["5777"];
+  let linksArray = [];
   
-  $('.Thumb--GridItem a').each((index, element) => {
+  /*$('.Thumb--GridItem a').each((index, element) => {
     const title = $(element).attr('title');
     const href = $(element).attr('href');
-    //linksArray.push({"title":title, "url":href});
-  )};
-  return linksArray;
+    linksArray.push({"title":title, "url":href});
+  )};*/
+   
+  const wiki = $('.Thumb--GridItem').text();
+  //const thumb = $('#mf-section-0').find('div > div > a > img').attr('src') || '//pngimg.com/uploads/wikipedia/wikipedia_PNG35.png';
+  //const judul = $('h1#section_0').text();
+  
+    
+  return wiki;
+
+
+
+
+
+  
+
+
+
+
 }
