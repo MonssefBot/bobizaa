eb17819851ce168199a1fb47b403da5c6bb45827
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-04-13 10:29:11 +0000

    Update timezone.js

--- plugins/timezone.js
+++ plugins/timezone.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone';
 
 const handler = async (m, {conn}) => {
   const tzAF = moment().tz('Africa/Casablanca').format('DD/MM/YY HH:mm:ss');
-  await conn.sendMessage(m.chat, {text: `الوقت الحالي في المغرب هو : \n${tzAF} \n
+  await conn.sendMessage(m.chat, {text: `الوقت الحالي في المغرب هو : \n*${tzAF}* \n
   ${String.fromCharCode(8206).repeat(850)}`}, {quoted: m});
 };
 handler.help = ["timezone"]
