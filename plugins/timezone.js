9819750c8f6550268242209ce6ab720c79f1ad3e
MonssefBot <162615452+MonssefBot@users.noreply.github.com>
2024-04-13 10:35:33 +0000

    Update timezone.js

--- plugins/timezone.js
+++ plugins/timezone.js
@@ -1,9 +1,8 @@
 import moment from 'moment-timezone';
 
 const handler = async (m, {conn}) => {
-  const tzAF = moment().tz('Africa/Casablanca').format('DD/MM/YY HH:mm:ss');
-  await conn.sendMessage(m.chat, {text: `الوقت الحالي في المغرب هو : \n*${tzAF}* \n
-  ${String.fromCharCode(8206).repeat(850)}`}, {quoted: m});
+  const tzAF = moment().tz('Africa/Casablanca').format('DD/MM/YYYY HH:mm:ss');
+  await conn.sendMessage(m.chat, {text: `الوقت الحالي في المغرب هو : \n*${tzAF}* \n${String.fromCharCode(8206).repeat(850)}`}, {quoted: m});
 };
 handler.help = ["timezone"]
 handler.tags = ["infobot"]
